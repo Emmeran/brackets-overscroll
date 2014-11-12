bc5a05f476435b6d92eb3db225bc9c1eb4eb2baa
Emmeran <->
2014-11-12 23:43:16 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Overscroll for Brackets
+A very simple extension for [Brackets](https://github.com/adobe/brackets/) to allow you to scroll below the document.
+
---
+++ main.js
@@ -0,0 +1,13 @@
+/*jslint vars: true, plusplus: true, devel: true, regexp: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define, brackets, $ */
+
+define(function (require, exports, module) {
+    "use strict";
+    
+    var AppInit = brackets.getModule("utils/AppInit");
+    
+    // Initialize extension
+    AppInit.appReady(function () {
+	    $("<style>.CodeMirror-sizer { margin-bottom: 1000px }</style>").appendTo("head");
+    });
+});
