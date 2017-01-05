9fd7f0ba5f0e82e39a0b89bed16bc02f20db0175
Lajos Meszaros <lajos.meszaros@performgroup.com>
2017-01-05 16:34:01 +0100

    Refactored the code a bit and added notes to remind me of the bug in the code

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 /*global define, brackets, $ */
 
 define(function (require, exports, module) {
-    'use strict';
+	'use strict';
     
 	function getAllPanes(){
 		return Array.from(document.querySelectorAll('#editor-holder .CodeMirror-wrap .CodeMirror-sizer>div'));
@@ -13,11 +13,39 @@ define(function (require, exports, module) {
 		return parseFloat(window.getComputedStyle(measure).lineHeight);
 	}
 	
+	/*
+	function getScrollAmount(pane){
+		return parseFloat(pane.style.top);
+	}
+	function setScrollAmount(pane, amount){
+		pane.style.top = amount + 'px';
+	}
+	
+	function getOffset(pane){
+		return parseFloat(pane.style.paddingBottom);
+	}
+	*/
+	function setOffset(pane, amount){
+		pane.style.paddingBottom = amount + 'px';
+	}
+	
 	var WorkspaceManager = brackets.getModule('view/WorkspaceManager');
 	
 	WorkspaceManager.on('workspaceUpdateLayout', function(event, newHeight){
 		getAllPanes().forEach(function(pane){
-			pane.style.paddingBottom = (newHeight - getLineHeight(pane) * 2) + 'px';
+			var lineHeight = getLineHeight(pane);
+			// var currentScroll = getScrollAmount(pane);
+			// var currentOffset = getOffset(pane);
+			var newOffset = (newHeight - lineHeight * 2);
+			
+			setOffset(pane, newOffset);
+			
+			// BUG: if we go from smaller screen to larger, then the editor 
+			// seemingly cannot remember the scroll position
+			// for example: we scrolled down as far as we can, top line on page
+			// is 132, if we upscale the window it will change to ~104
+			// the below line didn't work, I'm leaving it here for reference
+			// setScrollAmount(pane, currentScroll - currentOffset + newOffset);
 		});
 	});
 });
