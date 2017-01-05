9d62fdd174ff1f633b7e31940d6386bbf04e1e83
Lajos Meszaros <lajos.meszaros@performgroup.com>
2017-01-05 15:59:12 +0100

    Created exact measures and only allow scroll, until the file reaches the last line. Also added listener for window resizing

--- main.js
+++ main.js
@@ -2,12 +2,22 @@
 /*global define, brackets, $ */
 
 define(function (require, exports, module) {
-    "use strict";
+    'use strict';
     
-    var AppInit = brackets.getModule("utils/AppInit");
-    
-    // Initialize extension
-    AppInit.appReady(function () {
-	    $("<style>.CodeMirror-sizer { margin-bottom: 1000px !important }</style>").appendTo("head");
-    });
+	function getAllPanes(){
+		return Array.from(document.querySelectorAll('#editor-holder .CodeMirror-wrap .CodeMirror-sizer>div'));
+	}
+	
+	function getLineHeight(pane){
+		var measure = pane.querySelector('.CodeMirror-measure:first-child>pre>span');
+		return parseFloat(window.getComputedStyle(measure).lineHeight);
+	}
+	
+	var WorkspaceManager = brackets.getModule('view/WorkspaceManager');
+	
+	WorkspaceManager.on('workspaceUpdateLayout', function(event, newHeight){
+		getAllPanes().forEach(function(pane){
+			pane.style.paddingBottom = (newHeight - getLineHeight(pane) * 2) + 'px';
+		});
+	});
 });
